fb830efc3a769ffd2e3018b5a33115a9ba9a3a7d
Christian <alfredchristianzagre62@gmail.com>
2026-01-12 18:37:25 +0000

    Restructuration

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// Products Data
+        const commandeProducts = [
+            { name: 'Robe elegante', price: '2000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%23f5deb3" width="200" height="200"/></svg>' },
+            { name: 'Robe elegante', price: '2000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%23f5deb3" width="200" height="200"/></svg>' },
+            { name: 'Robe sexy', price: '4000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%234caf50" width="200" height="200"/></svg>' },
+            { name: 'Jeans homme', price: '7000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%232196f3" width="200" height="200"/></svg>' },
+            { name: 'Chemise en soie', price: '4000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%234169e1" width="200" height="200"/></svg>' },
+            { name: 'Robe elegante', price: '20000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%23f5deb3" width="200" height="200"/></svg>' },
+            { name: 'Robe elegante', price: '4000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%23000" width="200" height="200"/></svg>' },
+            { name: "Jeans patte d'elephe", price: '10000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%232196f3" width="200" height="200"/></svg>' }
+        ];
+
+        const fruitsProducts = [
+            { name: 'Concombre', price: '300 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%232d5016" width="200" height="200" rx="20"/></svg>' },
+            { name: 'Prune jaune', price: '600 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle cx="100" cy="100" r="80" fill="%23ffd700"/></svg>' },
+            { name: 'Tomate', price: '600 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle cx="100" cy="100" r="80" fill="%23ff0000"/></svg>' },
+            { name: 'Avocat', price: '500 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><ellipse cx="100" cy="100" rx="60" ry="80" fill="%2356a832"/></svg>' },
+            { name: 'Pomme de terre', price: '400 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><ellipse cx="100" cy="100" rx="70" ry="50" fill="%23c4a35a"/></svg>' },
+            { name: 'Roquette', price: '200 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%234caf50" width="200" height="200"/></svg>' },
+            { name: 'Figue', price: '400 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle cx="100" cy="100" r="80" fill="%238e4585"/></svg>' },
+            { name: 'Pomme', price: '800 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle cx="100" cy="100" r="80" fill="%23ff0000"/></svg>' }
+        ];
+
+        const artisanProducts = [
+            { name: 'Pagne Tisse', price: '15000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="%23ff6b35" width="200" height="200"/><rect fill="%23ffd700" x="20" y="20" width="160" height="20"/></svg>' },
+            { name: 'Chapeau de Saponé', price: '10000 Fcfa', img: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" '}
+        ];
